c1a36ee14a5deb5a1ba25c5b87f78358dadc6d2b
Evan-C-Mead <evan.c.mead@gmail.com>
2022-10-28 23:54:29 -0500

    conditional styling for boxes

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 export default function Die(props) {
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    }
+
     return (
-        <div className="die-face">
+        <div style={styles} className="die-face">
             <h2 className="die-number">{props.value}</h2>
         </div>
     )
